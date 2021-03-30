3cf173cf3d2a03972a019b0a0ebf1ac2e93ba87a
amurali <22786016+achmurali@users.noreply.github.com>
2021-03-30 17:15:56 +0530

    added routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import Routes from './Routes';
+import NavigationBar from './components/navigation-bar';
 
 const App = () => {
   return (
     <>
+    <NavigationBar/>
     <Routes />
     </>
   );
---
+++ client/src/components/navigation-bar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const NavigationBar:React.FC = () => {
+    return (
+        <AppBar>
+            <Toolbar>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default NavigationBar;
