f7a9a1b537d91fe06b24c8ff03833ca7000bb110
achmurali <achmurali@github.com>
2021-05-05 00:27:21 +0530

    Minor Updates

--- server/src/controllers/bug.ts
+++ server/src/controllers/bug.ts
@@ -14,6 +14,7 @@ import {
 } from "../db/queries/bug";
 import { checkRequestBody } from "../utils/validators";
 import Exception from "../models/Exception";
+import { Status, Priority } from '../models/Bugs'; 
 
 export const getAllBugs = asyncHandler(async (req: Request, res: Response) => {
   const projectId = req.params.projectId;
@@ -26,41 +27,34 @@ export const getAllBugs = asyncHandler(async (req: Request, res: Response) => {
 
 export const addBug = asyncHandler(async (req: Request, res: Response) => {
   const projectId = req.params.projectId;
-  const [name, description, status, priority] = checkRequestBody(req.body, [
-    "name",
-    "description",
-    "status",
-    "priority",
-  ]);
+  let [name, description, status, priority] = checkRequestBody(req.body, ["name","description","status","priority",]);
   const client = await dbConfig.getClient();
   try {
     const bugId = uuid();
-    const result = await client.query(addBugQuery, [
-      projectId,
-      req.user,
-      bugId,
-    ]);
-    if (result.rowCount != 1) throw new Exception("Something went wrong", 500);
-    const resultDetails = await client.query(addBugDetailsQuery, [
-      bugId,
-      name,
-      description,
-      req.user,
-      priority,
-      status
-    ]);
+    const result = await client.query(addBugQuery, [projectId,req.user,bugId,]);
+    
+    if (result.rowCount != 1)
+      throw new Exception("Something went wrong", 500);
+    
+    priority = getPriorityEnum(priority);
+    status = getStatusEnum(status);
+    const resultDetails = await client.query(addBugDetailsQuery, [bugId,name,description,req.user,priority,status]);
+    
     if (resultDetails.rowCount != 1)
       throw new Exception("Something went wrong", 500);
+    
     res.status(201).json({
       bug: result.rows,
       bugDetails: resultDetails.rows,
       success:true
     });
-  } catch (err) {
-    throw new Exception(err, 500);
-  } finally {
-    client.release();
-  }
+    }
+    catch (err) {
+      throw new Exception(err, 500);
+    } 
+    finally {
+      client.release();
+    }
 });
 
 export const deleteBug = asyncHandler(async (req: Request, res: Response) => {
@@ -104,9 +98,27 @@ const updateValues = (body:any,row:any,user:number) => {
     let newValues:any = {};
     newValues.name = body.name??row.name;
     newValues.description = body.description??row.description;
-    newValues.priority = body.priority??row.priority;
-    newValues.status = body.status??row.statuse;
+    newValues.priority = getPriorityEnum(body.priority??row.priority);
+    newValues.status = getStatusEnum(body.status??row.status);
     newValues.updatedtimestamp = new Date();
     newValues.updatedBy = user;
     return newValues;
+}
+
+const getStatusEnum = (status:string) => {
+  switch(status){
+    case "Close": return Status.CLOSE;
+    case "Open" : return Status.OPEN;
+    case "Idle" : return Status.IDLE;
+    default: throw new Exception("Bad Request",400);
+  }
+};
+
+const getPriorityEnum = (priority:string) => {
+  switch(priority){
+    case "High": return Priority.HIGH;
+    case "Medium": return Priority.MEDIUM;
+    case "Low": return Priority.LOW;
+    default: throw new Exception("Bad Request",400);
+  }
 }
---
+++ server/src/models/Bugs.ts
@@ -0,0 +1,11 @@
+export enum Status {
+    OPEN = "Open",
+    CLOSE = "Close",
+    IDLE = "Idle"
+};
+
+export enum Priority {
+    LOW = "Low",
+    MEDIUM = "Medium",
+    HIGH = "High"
+};
